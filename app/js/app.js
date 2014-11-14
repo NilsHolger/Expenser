04c6553320a39ce463dc59776d886942982e3548
NilsHolger <nils@nilsnaegele.com>
2014-11-14 18:25:54 +0100

    First commit

--- app/js/app.js
+++ app/js/app.js
@@ -7,10 +7,25 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
+  'ngTouch',
+  'ngAnimate'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$routeProvider',
+ function($routeProvider) {
+  $routeProvider.when('/',{
+  	templateUrl: 'partials/home.html',
+  	 controller: 'HomeCtrl'
+  	});	
+  $routeProvider.when('/expense-add',{
+  	templateUrl: 'partials/expense-add.html',
+  	 controller: 'ExpenseAddCtrl'
+  	});
+  $routeProvider.when('/summary-view', {
+  	templateUrl: 'partials/summary-view.html',
+  	 controller: 'SummaryViewCtrl'
+  	});
+  $routeProvider.otherwise({
+  	redirectTo: '/'
+  });
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,48 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+.controller('HomeCtrl', ['$scope', function($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+}])
+.controller('ExpenseAddCtrl', ['$scope', 'categoryList', 'expenseService',
+	function($scope, categoryList, expenseService) {
+		$scope.categories = categoryList;
+		$scope.submit = function(){
+			expenseService.saveExpense($scope.expense);
+		}
+	}])
+.controller('SummaryViewCtrl', ['$scope', 'categoryList', 'expenseService', function($scope, categoryList, expenseService) {
+	$scope.expenses = expenseService.getExpense();
+	$scope.summaryData = [];
+	var categories=categoryList;
+	categories.forEach(function(item) {
+		var catTotal = expenseService.getCategoryTotal(item);
+		$scope.summaryData.push({
+			category: item,
+			amount: catTotal
+		});
 
-  }]);
+	});
+}])
+.controller('NavigationCtrl',['$scope' ,'$location',function($scope,$location){
+  var navigator = function(incrementer) {
+    var pages = ['/', '/expense-add', '/summary-view'];
+   $scope.slidingDirection = (incrementer === 1) ? 'slide-right' : 'slide-left';
+    var nextUrl = "";
+    var currentPage = $location.path();
+    var lastPageIndex = pages.length - 1;
+    var pageIndex = pages.indexOf(currentPage);
+    var direction = pageIndex + incrementer;
+    if (direction === -1) direction = lastPageIndex;
+    if (direction > lastPageIndex) direction = 0;
+    nextUrl = pages[direction];
+    $location.url(nextUrl);
+  };
+  $scope.goLeft = function() {
+    navigator(-1);
+};
+$scope.goRight = function() {
+    navigator(1);
+};
+}])
+
